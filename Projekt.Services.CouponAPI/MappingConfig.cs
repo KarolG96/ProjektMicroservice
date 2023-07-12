003398a2d0bf02928006593f8298a8a56354a2b4
KarolG96 <karol.g.96@icloud.com>
2023-07-12 15:21:14 +0200

    12.07_db

---
+++ Projekt.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Projekt.Services.CouponAPI.Models.Dto;
+
+namespace Projekt.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, CouponDto>();
+                config.CreateMap<CouponDto, CouponDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
